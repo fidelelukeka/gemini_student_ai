305f67739a95dc7ffab001287bef2d3869c40ca8
Fidèle Lukeka <fidelelukeka@gmail.com>
2025-07-17 12:50:04 +0200

    Correcting some deployment issue

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ CORS(app)
 firebase_config = {
     "type": os.getenv("FIREBASE_TYPE"),
     "project_id": os.getenv("FIREBASE_PROJECT_ID"),
-    "private_key_id": os.getenv("FIREBASE_PRIVATE_KEY_ID"),
+    "private_key_id": os.getenv("FIREBASE_PRIVATE_KEY_ID").replace('\\n', '\n'),
     "private_key": os.getenv("FIREBASE_PRIVATE_KEY").replace('\\n', '\n'),
     "client_email": os.getenv("FIREBASE_CLIENT_EMAIL"),
     "client_id": os.getenv("FIREBASE_CLIENT_ID"),
